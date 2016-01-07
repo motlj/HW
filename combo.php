6494b632ccb06bc8136c1122fbfece6054cf2bbe
Joshua Motl <joshuadamotl@gmail.com>
2016-01-07 02:08:59 +0000

    Added assignment 5 for hw/w2-d2

---
+++ combo.php
@@ -0,0 +1,19 @@
+<?php
+
+function instructions($ins1,$ins2,$ins3,$ins4,$ins5,$ins6,$ins7,$ins8,$ins9,$ins10,$ins11){
+	echo $ins1."\n";
+	echo $ins2."\n";
+	echo $ins3."\n";
+	echo $ins4."\n";
+	echo $ins5."\n";
+	echo $ins6."\n";
+	echo $ins7."\n";
+	echo $ins8."\n";
+	echo $ins9."\n";
+	echo $ins10."\n";
+	echo $ins11."\n";
+}
+
+echo instructions("Up","Up","Down","Down","Left","Right","Left","Right","B","A","Start");
+
+?>
